00f7e40731be602a7f3a5dc7768dec2cae5d0b7a
VICKY <vishujoshi21nov2000@gmail.com>
2023-03-05 10:37:23 +0530

    B Solved

---
+++ AtCoder Beginner Contest 292/b.cpp
@@ -0,0 +1,75 @@
+// KNOCKCAT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                          \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define int long long int
+#define endl "\n"
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9 + 7;
+const ll eps = 1e-9;
+const ll maxn = 1e5 + 1;
+const ll inf = 5e18;
+const ll minf = -inf;
+
+// This is to print to correct decimal places
+// cout << fixed << setprecision(10) << ans << endl;
+
+int32_t main()
+{
+    fast;
+
+    // int tt;
+    // cin >> tt;
+
+    // while (tt--)
+    // {
+    //     // knockcat
+
+    // }
+
+    int n, q;
+    cin >> n >> q;
+    int event, a;
+    map<int, int> mp;
+    for (int i = 1; i <= n; ++i)
+    {
+        mp.insert({i, 1});
+    }
+    // for (auto itr : mp)
+    // {
+    //     cout << itr.first << " " << itr.second << endl;
+    // }
+    while (q--)
+    {
+        cin >> event;
+        cin >> a;
+        if (event == 1)
+        {
+            ++mp[a];
+            if (mp[a] >= 3)
+            {
+                mp.erase(a);
+            }
+        }
+        else if (event == 2)
+        {
+            mp.erase(a);
+        }
+        if (event == 3)
+        {
+            if (mp.find(a) != mp.end())
+                cout << "No" << endl;
+            else
+                cout << "Yes" << endl;
+        }
+    }
+    return 0;
+}
